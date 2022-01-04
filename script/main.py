4dba2969ae15cc64bd1f4f75bebdbd7854f151f1
altair0708 <960808225@qq.com>
2022-01-04 22:35:02 +0800

    20220104 displacement interpolation

--- script/main.py
+++ script/main.py
@@ -1,13 +1,11 @@
-import matplotlib.pyplot as plt
 from NMM.control.ElementCreator import ElementCreator
-from NMM.fem.PatchWithDataBase import get_patch_number
 from NMM.control.MatrixAssembly import MatrixAssembler
 from NMM.control.ElementRefresh import ElementRefresher
 from NMM.preprocess.CvFileReader import CvFileReader
 from NMM.plot.PlotPatchDisplacement import plot_joint_displacement
 from scipy.sparse.linalg import spsolve
 
-cv_filename = 'cv00'
+cv_filename = 'cvUniaxialCompression01'
 database_name = ':memory:'
 work_path = '../data/'
 
@@ -16,13 +14,12 @@ cv_reader.run()
 database_cursor = cv_reader.cursor
 
 element_creator = ElementCreator(database_cursor)
-patch_number = get_patch_number(database_cursor)
 temp_element_list = element_creator.start()
 
 for i in range(10):
     print('Step: {step}'.format(step=i + 1))
-    stiff_matrix = MatrixAssembler.stiff_matrix(temp_element_list, patch_number)
-    force_vector = MatrixAssembler.force_vector(temp_element_list, patch_number)
+    stiff_matrix = MatrixAssembler.stiff_matrix(temp_element_list, database_cursor)
+    force_vector = MatrixAssembler.force_vector(temp_element_list, database_cursor)
 
     x = spsolve(stiff_matrix, force_vector)
 
