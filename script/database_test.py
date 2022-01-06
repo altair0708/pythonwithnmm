95f2001c193e31ed2c1f95bb084c844f160c2e0e
altair0708 <960808225@qq.com>
2022-01-06 17:28:44 +0800

    20220106 uniaxial compression

--- script/database_test.py
+++ script/database_test.py
@@ -3,7 +3,7 @@ import sqlite3
 database_name = '../data/test.db'
 with sqlite3.connect(database_name) as connection:
     database_cursor = connection.cursor()
-    database_statement = 'SELECT * FROM PhysicalPatches WHERE (xValue, yValue) in ' \
+    database_statement = 'SELECT * FROM JointPoints WHERE (xValue, yValue) in ' \
                          '(SELECT xValue, yValue FROM PhysicalPatches GROUP BY xValue, yValue HAVING COUNT(*) > 1)'
 
     result = database_cursor.execute(database_statement)
--- script/main.py
+++ script/main.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
+
 from NMM.control.ElementCreator import ElementCreator
 from NMM.control.MatrixAssembly import MatrixAssembler
 from NMM.control.ElementRefresh import ElementRefresher
 from NMM.preprocess.CvFileReader import CvFileReader
-from NMM.plot.PlotPatchDisplacement import plot_joint_displacement
+from NMM.plot.PlotPatchDisplacement import plot_joint_x_displacement, plot_joint_y_displacement
 from scipy.sparse.linalg import spsolve
 
 cv_filename = 'cvUniaxialCompression01'
@@ -16,7 +18,9 @@ database_cursor = cv_reader.cursor
 element_creator = ElementCreator(database_cursor)
 temp_element_list = element_creator.start()
 
-for i in range(10):
+plt.ion()
+fig: plt.Figure = plt.figure(figsize=(16, 9))
+for i in range(100):
     print('Step: {step}'.format(step=i + 1))
     stiff_matrix = MatrixAssembler.stiff_matrix(temp_element_list, database_cursor)
     force_vector = MatrixAssembler.force_vector(temp_element_list, database_cursor)
@@ -27,5 +31,13 @@ for i in range(10):
     ElementRefresher.refresh_patch_displacement(x, temp_element_list, database_cursor)
     ElementRefresher.refresh_joint_displacement(temp_element_list, database_cursor)
 
-plot_joint_displacement(database_cursor)
+    fig.clf()
+    ax1: plt.Axes = fig.add_subplot(121)
+    ax2: plt.Axes = fig.add_subplot(122)
+    plot_joint_x_displacement(database_cursor, ax1)
+    plot_joint_y_displacement(database_cursor, ax2)
+    plt.pause(0.01)
+
+plt.ioff()
+plt.show()
 cv_reader.close()
